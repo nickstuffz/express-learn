caad68ecdf08b4db30c5746242b44ce70890fdab
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2024-12-13 10:58:14 -0800

    ejs

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const path = require("node:path");
 const app = express();
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,17 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("home page"));
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+indexRouter.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+});
+
 indexRouter.get("/about", (req, res) => {
   res.send("about page");
 });
